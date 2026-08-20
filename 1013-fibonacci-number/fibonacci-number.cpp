dc8fcca2cf890a4e76e468dfd7781f4d248e8414
AdiBoT <adityasuryan467@gmail.com>
2026-08-20 10:33:05 +0530

    Time: 2 ms (77.48%) | Memory: 7.8 MB (38.73%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        if (n <= 1)
+            return n;
+        long long a = 0;
+        long long b = 1;
+        long long sum;
+        for (int i = 2; i <= n; i++) {
+            sum = a + b;
+            a=b;
+            b=sum;
+        }
+        return sum;
     }
 };
